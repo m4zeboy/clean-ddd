764a2f5980418795e70af241cab1625a36b4d508
m4zeboy <moises775azevedo@gmail.com>
2023-12-28 11:46:25 -0300

    Value Objects

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,17 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
   constructor(
@@ -17,6 +20,7 @@ export class Question {
   ) {
     this.id = id ?? randomUUID()
     this.title = props.title
+    this.slug = props.slug
     this.content = props.content
     this.authorId = props.authorId
   }
---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test('it should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Slug Create Test')
+
+  expect(slug.value).toEqual('slug-create-test')
+})
