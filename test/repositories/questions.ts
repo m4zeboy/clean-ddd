71f4b5f536d7c0b4d3d50b12c6666c057a343273
m4zeboy <moises775azevedo@gmail.com>
2024-01-01 15:49:39 -0300

    Caso de uso: Listar perguntas recentes

--- test/repositories/questions.ts
+++ test/repositories/questions.ts
@@ -1,9 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
-
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id)
     if (!question) return null
@@ -14,6 +14,14 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const question = this.items.find((item) => item.slug.value === slug)
     if (!question) return null
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
