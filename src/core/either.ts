852c918b46d98b79875e404b2e0e7fbd800df7ba
m4zeboy <moises775azevedo@gmail.com>
2024-01-04 09:39:29 -0300

    Testando classes de erro

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,20 +1,36 @@
-export class Failure<F> {
-  readonly reason: F
+export class Failure<F, S> {
+  readonly value: F
 
-  constructor(reason: F) {
-    this.reason = reason
+  constructor(value: F) {
+    this.value = value
+  }
+
+  isFailure(): this is Failure<F, S> {
+    return true
+  }
+
+  isSuccess(): this is Success<F, S> {
+    return false
   }
 }
 
-export class Success<S> {
+export class Success<F, S> {
   readonly value: S
 
   constructor(value: S) {
     this.value = value
   }
+
+  isFailure(): this is Failure<F, S> {
+    return false
+  }
+
+  isSuccess(): this is Success<F, S> {
+    return true
+  }
 }
 
-export type Either<F, S> = Failure<F> | Success<S>
+export type Either<F, S> = Failure<F, S> | Success<F, S>
 
 export const failure = <F, S>(reason: F): Either<F, S> => {
   return new Failure(reason)
