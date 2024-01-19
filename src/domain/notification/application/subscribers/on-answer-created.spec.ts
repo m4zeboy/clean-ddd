e28c920b0be6bb6945d4eb39fc3f460dc656d5bd
m4zeboy <moises775azevedo@gmail.com>
2024-01-19 13:53:15 -0300

    Enviando notificação no subscriber

--- src/domain/notification/application/subscribers/on-answer-created.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -2,19 +2,56 @@ import { makeAnswer } from 'test/factories/make-answer'
 import { OnAnswerCreated } from './on-answer-created'
 import { InMemoryAnswersRepository } from 'test/repositories/answers'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/answer-attachments'
+import { InMemoryQuestionsRepository } from 'test/repositories/questions'
+import {
+  SendNotificationUseCase,
+  SendNotificationUseCaseRequest,
+  SendNotificationUseCaseResponse,
+} from '../use-cases/send-notification'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/question-attachments'
+import { InMemoryNotificationsRepository } from 'test/repositories/notifications'
+import { makeQuestion } from 'test/factories/make-question'
+import { SpyInstance } from 'vitest'
+import { waitFor } from 'test/utils/wait-for'
 
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sendNotification: SendNotificationUseCase
 
-describe('on answer created', () => {
+let sendNotificationExecuteSpy: SpyInstance<
+  [SendNotificationUseCaseRequest],
+  Promise<SendNotificationUseCaseResponse>
+>
+
+describe('on answer created', async () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       new InMemoryAnswerAttachmentsRepository(),
     )
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      new InMemoryQuestionAttachmentsRepository(),
+    )
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sendNotification = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+    )
+    sendNotificationExecuteSpy = vi.spyOn(sendNotification, 'execute')
+    new OnAnswerCreated(inMemoryQuestionsRepository, sendNotification)
   })
 
-  it.only('shout send a notification when an answer is created', () => {
-    const onAnswerCreated = new OnAnswerCreated()
-    const answer = makeAnswer()
+  it.only('shout send a notification when an answer is created', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+    inMemoryQuestionsRepository.create(question)
+
     inMemoryAnswersRepository.create(answer)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
   })
 })
